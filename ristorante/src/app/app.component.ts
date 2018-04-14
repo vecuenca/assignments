0b81512f6ed69210a1e957061df159fb7f706c6f
Vincent E. Cuenca <vincent.e.cuenca@avanade.com>
2018-04-14 18:53:57 -0400

    add databinding for menu

--- ristorante/src/app/app.component.ts
+++ ristorante/src/app/app.component.ts
@@ -6,6 +6,8 @@ import { MENU } from './mock-menu';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+  menu = MENU;
 }
